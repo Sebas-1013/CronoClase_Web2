bd6b1edcbb8ddeab59fb89db1b21a113f60311b4
irwincol <irwincolmenarez10@gmail.com>
2026-04-21 21:48:20 -0500

    (chore) implementación de la página de inicio de sesión para profesores y configuración de rutas de API

--- src/services/api.js
+++ src/services/api.js
@@ -10,6 +10,9 @@ export let end_points={
 
 export let fakeRoutes = {
 
+    logInProfesor:
+    "https://raw.githubusercontent.com/GGP113/CronoClase_Web2/refs/heads/master/src/data/profesor.json",
+
     logInEstudiante: "https://raw.githubusercontent.com/GGP113/CronoClase_Web2/refs/heads/develop/creacion-registros/src/data/estudiantes.json"
 
 
